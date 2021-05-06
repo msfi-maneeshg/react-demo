43394a8ceaa9a72d1c33581ea8dff2bf70cf2f96
msfi-maneeshg <maneeshg@mindfiresolutions.com>
2021-05-06 11:14:43 +0530

    update the code

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -16,46 +16,60 @@ class Registration extends React.Component{
             password : inputFieldComponent
         }
     }
+    checkValueType(e){
+        var numberPattern = new RegExp(/^[0-9]+$/i);
 
-    getFormValue(e){
+        if (e.key !== "Backspace"&& !numberPattern.test(e.key)) {
+            e.preventDefault()
+        }
+    }
+
+    setFormValue(e){
         let fName = e.target.name.toString();
         let inputFieldComponent = {
             value :e.target.value,
             isValid : true,
             errorMessage : ''
         };
+
         if(fName === 'name'){
             this.setState({name:inputFieldComponent});
         }else if(fName === 'email'){
             this.setState({email:inputFieldComponent});
-        }else if(fName === 'phone'){
+        }else if(fName === 'phone' && inputFieldComponent.value.length <= 10){
             this.setState({phone:inputFieldComponent});
         }else if(fName === 'password'){
             this.setState({password:inputFieldComponent});
         }
 
     }
+
     submitRegistrationDetail(e){
         //-------- Name validation
         if(!this.state.name.value){
-            this.setState({name:{isValid:false,errorMessage:'Full Name can not be empty'}});
+            this.setState({name:{value:this.state.name.value,isValid:false,errorMessage:'Full Name can not be empty'}});
         }
 
         //-------- Email validation
         if(!this.state.email.value){
-            this.setState({email:{isValid:false,errorMessage:'Email Address can not be empty'}});
+            this.setState({email:{value:this.state.email.value,isValid:false,errorMessage:'Email Address can not be empty'}});
+        }else{
+        var emailPattern = new RegExp(/^[a-zA-Z0-9+_.-]+[@]+[a-zA-Z0-9.-]+[.]+[a-zA-Z0-9.-]+$/i);
+
+        if (!emailPattern.test(this.state.email.value)) {
+            this.setState({email:{value:this.state.email.value,isValid:false,errorMessage:'Please enter valid email address.'}});
         }
+    }
 
         //-------- Phone validation
         if(!this.state.phone.value){
-            this.setState({phone:{isValid:false,errorMessage:'Phone Number can not be empty'}});
+            this.setState({phone:{value:this.state.phone.value,isValid:false,errorMessage:'Phone Number can not be empty'}});
         }
 
         //-------- Password validation
         if(!this.state.password.value){
-            this.setState({password:{isValid:false,errorMessage:'Password can not be empty'}});
+            this.setState({password:{value:this.state.name.value,isValid:false,errorMessage:'Password can not be empty'}});
         }
-        console.log(this.state);
         e.preventDefault();
     }
     render(){
@@ -65,29 +79,31 @@ class Registration extends React.Component{
                 <div>
                     <form onSubmit={(e) => this.submitRegistrationDetail(e) }>
                         <table>
-                            <tr>
-                                <td><label>Full Name:</label></td>
-                                <td><input type="text" name="name" value={this.state.name.value} onChange={(e) => this.getFormValue(e)}/></td>
-                                <td><ValidationError filedInfo={this.state.name}/></td>
-                            </tr>
-                            <tr>
-                                <td><label>Email Address:</label></td>
-                                <td><input type="text" name="email" value={this.state.email.value} onChange={(e) => this.getFormValue(e)}/></td>
-                                <td><ValidationError filedInfo={this.state.email}/></td>
-                            </tr>
-                            <tr>
-                                <td><label>Phone Number:</label></td>
-                                <td><input type="text" name="phone" value={this.state.phone.value} onChange={(e) => this.getFormValue(e)}/></td>
-                                <td><ValidationError filedInfo={this.state.phone}/></td>
-                            </tr>
-                            <tr>
-                                <td><label>Password:</label></td>
-                                <td><input type="password" name="password" value={this.state.password.value} onChange={(e) => this.getFormValue(e)}/></td>
-                                <td><ValidationError filedInfo={this.state.password}/></td>
-                            </tr>
-                            <tr>
-                                <td><input type="submit" value="Submit Details"/></td>
-                            </tr>
+                            <tbody>
+                                <tr>
+                                    <td><label>Full Name:</label></td>
+                                    <td><input type="text" name="name" value={this.state.name.value} onChange={(e) => this.setFormValue(e)}/></td>
+                                    <td><ValidationError filedInfo={this.state.name}/></td>
+                                </tr>
+                                <tr>
+                                    <td><label>Email Address:</label></td>
+                                    <td><input type="text" name="email" value={this.state.email.value} onChange={(e) => this.setFormValue(e)}/></td>
+                                    <td><ValidationError filedInfo={this.state.email}/></td>
+                                </tr>
+                                <tr>
+                                    <td><label>Phone Number:</label></td>
+                                    <td><input type="text" name="phone" value={this.state.phone.value} onChange={(e) => this.setFormValue(e)} onKeyDown={(e)=>this.checkValueType(e)}/></td>
+                                    <td><ValidationError filedInfo={this.state.phone}/></td>
+                                </tr>
+                                <tr>
+                                    <td><label>Password:</label></td>
+                                    <td><input type="password" name="password" value={this.state.password.value} onChange={(e) => this.setFormValue(e)}/></td>
+                                    <td><ValidationError filedInfo={this.state.password}/></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="submit" value="Submit Details"/></td>
+                                </tr>
+                            </tbody>
                         </table>
                     </form>
                 </div>
